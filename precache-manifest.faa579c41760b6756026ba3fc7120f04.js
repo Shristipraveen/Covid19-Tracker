6072458725dbb8fa40a8aac6f1d190d86679192d
Shristi <shristiprave@gmail.com>
2020-08-16 12:23:51 +0530

    Updates

---
+++ precache-manifest.faa579c41760b6756026ba3fc7120f04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e544b9bb2eb8e97301afaa29b749cb7d",
+    "url": "/Covid19-Tracker/index.html"
+  },
+  {
+    "revision": "d37f5406566d4ba6e8b3",
+    "url": "/Covid19-Tracker/static/css/main.57c45e1a.chunk.css"
+  },
+  {
+    "revision": "bad72be175d1f0ad9070",
+    "url": "/Covid19-Tracker/static/js/2.cb852d13.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid19-Tracker/static/js/2.cb852d13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d37f5406566d4ba6e8b3",
+    "url": "/Covid19-Tracker/static/js/main.8d159d21.chunk.js"
+  },
+  {
+    "revision": "82f82ba8ddcb86e25f57",
+    "url": "/Covid19-Tracker/static/js/runtime-main.15390705.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19-Tracker/static/media/Covid19.d7265326.png"
+  }
+]);
